3a947495691f0979fc2e84ee0ea805f535c7939c
AlfaRojo <kortiz@correo.url.edu.gt>
2019-03-06 08:55:50 -0600

    Estructura de arbol Binario Genérico 100%

---
+++ EstrucutrasNoLin/ArbolBinarioBusqueda.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstrucutrasNoLin
+{
+    class ArbolBinarioBusqueda<T> : iEstructuraNoLineales<T>, IEnumerable<T> where T : IComparable
+    {
+        public cNodo<T> Raiz { get; set; }
+        public void Buscar(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ArbolBinarioBusqueda(cNodo<T> Raiz)
+        {
+            this.Raiz = Raiz;
+        }
+        public void Insertar(T value)
+        {
+            if (Raiz == null)
+            {
+                Raiz = new cNodo<T>(value);//Se manda la información a la raiz
+            }
+            else
+            {
+                cNodo<T> Temporal = Raiz;
+                bool Encontrado = false;
+                while (!Encontrado)//Recorrido del arbol
+                {
+                    int ValueComp = Temporal.sInformacion.CompareTo(value);//Comprara para devolver valores de -1 y +1
+                    if (ValueComp < 0)//Valores -1 van a la izquierda
+                    {
+                        if (Temporal.nIzquierda == null)
+                        {
+                            Temporal.nIzquierda = new cNodo<T>(value, Temporal);
+                            return;
+                        }
+                        else
+                        {
+                            Temporal = Temporal.nIzquierda;
+                        }
+                    }
+                    else if (ValueComp > 0)//Valores +1 van a la derecha
+                    {
+                        if (Temporal.nDerecha == null)
+                        {
+                            Temporal.nDerecha = new cNodo<T>(value, Temporal);
+                            return;
+                        }
+                        else
+                        {
+                            Temporal = Temporal.nDerecha;
+                        }
+                    }
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EstrucutrasNoLin/Nodos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstrucutrasNoLin
+{
+    class cNodo<T>
+    {
+        public T sInformacion { get; set; }
+        public cNodo<T> nDerecha { get; set; }
+        public cNodo<T> nIzquierda { get; set; }
+        public cNodo<T> nPadre { get; set; }
+        public bool Raiz { get { return nPadre == null; } }
+        public bool Hoja { get { return nIzquierda == null && nDerecha == null; } }
+        public cNodo(T value, cNodo<T> nPadre, cNodo<T> nIzquierda ,cNodo<T> nDerecha)
+        {
+            sInformacion = value;
+            this.nDerecha = nDerecha;
+            this.nIzquierda = nIzquierda;
+            this.nPadre = nPadre;
+        }
+        public cNodo(T value)
+        {
+            sInformacion = value;
+        }
+        public cNodo(T value, cNodo<T> nPadre)
+        {
+            sInformacion = value;
+            this.nPadre = nPadre;
+        }
+    }
+}
