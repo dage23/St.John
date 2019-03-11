7c9b87dd2a7d7ac58beebd9c2571d8b47697cae5
dage23 <46784507+dage23@users.noreply.github.com>
2019-03-11 10:00:00 -0600

    Creacion de Pedido

--- St.John/Controllers/FarmaController.cs
+++ St.John/Controllers/FarmaController.cs
@@ -6,14 +6,15 @@ using System.Web.Mvc;
 using System.IO;
 using St.John.Models;
 using System.Text.RegularExpressions;
-
+using EstrucutrasNoLin;
+using St.John.Gelpers;
 namespace St.John.Controllers
 {
     public class FarmaController : Controller
     {
         public ActionResult Index()
         {
-            return View(new List<DatosFarma>());
+            return View(new List<DatosFarma>());            
         }
         [HttpPost]
         public ActionResult Index(HttpPostedFileBase postedFile)
@@ -34,32 +35,42 @@ namespace St.John.Controllers
 
                 string csvData = System.IO.File.ReadAllText(filePath);
                 Regex CSV = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                bool bandera = true;
                 foreach (string row in csvData.Split('\n'))
                 {
                     if (!string.IsNullOrEmpty(row))
                     {
                         String[] fields = CSV.Split(row);
-                        for (int i = 0; i < 1; i++)
+                        if (!bandera)
                         {
-                            fields[i] = fields[i].Trim();
-                            string Id = fields[0];
-                            string nombre = fields[1];
-                            string descripcion = fields[2];
-                            string casa = fields[3];
-                            string precio = fields[4];
-                            string existecia = fields[5];
-                            ListaDrogas.Add(new DatosFarma
+                            for (int i = 0; i < 1; i++)
                             {
-                                Codigo = Id,
-                                Nombre = nombre,
-                                Descricpion = descripcion,
-                                Origen = casa,
-                                Precio = precio,
-                                Existencia = existecia
-                            });
-                        }
-                        
+                                fields[i] = fields[i].Trim();
+                                string Id = fields[0];
+                                string nombre = fields[1];
+                                string descripcion = fields[2];
+                                string casa = fields[3];
+                                string precio = fields[4];
+                                string existecia = fields[5];
+                                ListaDrogas.Add(new DatosFarma
+                                {
+                                    Codigo = Id,
+                                    Nombre = nombre,
+                                    Descricpion = descripcion,
+                                    Origen = casa,
+                                    Precio = precio,
+                                    Existencia = existecia
+                                });
+                                var DrogaActual = new DatosFarma
+                                {
+                                    Nombre = nombre,
+                                    Codigo = Id
+                                };
+                                Datos.Instance.ArbolDrogas.Insertar(DrogaActual);
+                            }
+                        }                           
                     }
+                    bandera = false;
                 }
             }
             return View(ListaDrogas);
@@ -117,6 +128,24 @@ namespace St.John.Controllers
         // POST: Farma/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Pedido()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Pedido(FormCollection collection)
         {
             try
             {
---
+++ St.John/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace St.John.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "Nombre de Cliente")]
+        public string NombreCliente { get; set; }
+        [Display(Name = "Direccion de Cliente")]
+        public string DireccionCliente { get; set; }
+        [Display(Name = "NIT de Cliente")]
+        public string NitCliente { get; set; }
+        [Display(Name = "Nombre de Droga")]
+        public string DrogaCliente { get; set; }
+        [Display(Name = "Total de Pedido")]
+        public string TotalCliente { get; set; }
+    }
+}
